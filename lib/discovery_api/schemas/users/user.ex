81eabe3135e4797b08f5da3e6dbfb31f9770c49f
Jake Prem <jakeprem@gmail.com>
2019-11-04 15:58:18 -0500

    migration to add generated public ID to users table
    smartcitiesdata/discovery_api#14
    
    co-authored-by: Jake Prem <jakeprem@gmail.com>

--- lib/discovery_api/schemas/users/user.ex
+++ lib/discovery_api/schemas/users/user.ex
@@ -12,6 +12,7 @@ defmodule DiscoveryApi.Schemas.Users.User do
   schema "users" do
     field(:subject_id, :string)
     field(:email, :string)
+    field(:public_id, Ecto.UUID)
     has_many(:visualizations, Visualization, foreign_key: :owner_id)
     many_to_many(:organizations, Organization, join_through: DiscoveryApi.Schemas.Users.UserOrganization)
 
