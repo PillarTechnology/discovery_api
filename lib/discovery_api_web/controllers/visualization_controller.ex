4932d7e05d1c5cb727654ece7bb747518ff0bd5e
Timothy Regan <tregan@pillartechnology.com>
2019-11-08 10:27:41 -0500

    Router, ldap auth, and tests
    smartcitiesdata/react_discovery_ui#70

--- lib/discovery_api_web/controllers/visualization_controller.ex
+++ lib/discovery_api_web/controllers/visualization_controller.ex
@@ -4,16 +4,35 @@ defmodule DiscoveryApiWeb.VisualizationController do
 
   alias DiscoveryApi.Schemas.Users
   alias DiscoveryApi.Schemas.Visualizations
+  alias DiscoveryApiWeb.Utilities.AuthUtils
 
   plug(:accepts, DiscoveryApiWeb.VisualizationView.accepted_formats())
 
   def show(conn, %{"id" => id}) do
+<<<<<<< HEAD
     case Visualizations.get_visualization_by_id(id) do
       {:error, _} -> render_error(conn, 404, "Not Found")
       {:ok, visualization} -> render(conn, :visualization, %{visualization: visualization})
     end
   end
 
+=======
+    with {:ok, %{query: query} = visualization} <- Visualizations.get_visualization(id),
+    user <- Map.get(conn.assigns, :current_user, :anonymous_user) |> IO.inspect(),
+         true <- AuthUtils.authorized_to_query?(query, user) do
+
+      render(conn, :visualization, %{visualization: visualization})
+    else
+      {:error, _} -> render_error(conn, 404, "Not Found")
+      false -> render_error(conn, 404, "Not Found")
+    end
+  end
+
+  # defp render_authorized_visualization(conn, {:error, _}), do: render_error(conn, 404, "Not Found")
+
+  # defp render_authorized_visualization(conn, {:ok, visualization}), do: render(conn, :visualization, %{visualization: visualization})
+
+>>>>>>> Router, ldap auth, and tests
   def create(conn, %{"query" => query, "title" => title}) do
     with {:ok, user} <- Users.get_user(conn.assigns.current_user, :subject_id),
          {:ok, visualization} <- Visualizations.create(%{query: query, title: title, owner: user}) do
