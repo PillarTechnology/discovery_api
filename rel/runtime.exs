87c3bfe619201ddab98d8241c4a00a60fd6d3c7a
ryan.r.conaway <Ryan@conaway58.com>
2019-11-20 12:20:00 -0500

    fixed runtime

--- rel/runtime.exs
+++ rel/runtime.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 redis_host = System.get_env("REDIS_HOST")
-redis_password = System.get_env("REDIS_PASSWORD", "")
+redis_password = System.get_env("REDIS_PASSWORD")
 all_redis_args = [host: redis_host, password: redis_password]
 redix_args = Enum.filter(all_redis_args, fn
 	{_, nil} -> false
